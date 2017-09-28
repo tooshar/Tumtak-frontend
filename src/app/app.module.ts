c4134a86dea654156850fdd63eb9a94f6b3af616
tooooshar <tusharprajapati@venturepact.com>
2017-09-28 21:35:27 +0530

    material design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatTabsModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
